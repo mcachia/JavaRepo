8fac8e3d50ced9b9a24b4a7e63b8f230f5a8e6d6
mcachia <mcachia1981@gmail.com>
2016-05-27 11:52:51 +0200

    Milestone1-HW

--- src/HomeWork/Milestone1_Methods/CalculateArea.java
+++ src/HomeWork/Milestone1_Methods/CalculateArea.java
@@ -3,15 +3,29 @@ package HomeWork.Milestone1_Methods;
 import java.util.Scanner;
 
 /**
+ * M1: create methods to calculate area of rectangle, square, triangle, circle
+ * in main method, ask user for a selection, depending what he enters, ask the right questions
+ * (eg radius for circle, length and breadth for rectangle), and compute and return the right area
  * Created by mcachia on 27/05/2016.
  */
 public class CalculateArea {
     public static void main(String[] args) {
-
-
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter choice below to check Area: \n1.Rectangle\n2.Triangle\n3.Square\n4.Circle");
+        int areaCalculation = userInput.nextInt();
+        CalculateArea ca = new CalculateArea();
+        if (areaCalculation == 1) {
+            ca.rectangle();
+        } else if (areaCalculation == 2) {
+            ca.triangle();
+        } else if (areaCalculation == 3) {
+            ca.square();
+        } else if (areaCalculation == 4) {
+            ca.circle();
+        }
     }
 
-    public static void rectangle() {
+    public void rectangle() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter length:");
         double length = input.nextDouble();
@@ -21,7 +35,7 @@ public class CalculateArea {
         System.out.println("The total area for your rectangle is: " + areaRectangle);
     }
 
-    public static void triangle() {
+    public void triangle() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter base:");
         double base = input.nextDouble();
@@ -31,7 +45,7 @@ public class CalculateArea {
         System.out.println("The total area for your triangle is: " + areaTriangle);
     }
 
-    public static void square() {
+    public void square() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter side length:");
         double side = input.nextDouble();
@@ -39,7 +53,7 @@ public class CalculateArea {
         System.out.println("The total area for your square is: " + areaSquare);
     }
 
-    public static void circle() {
+    public void circle() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter radius:");
         double radius = input.nextDouble();
