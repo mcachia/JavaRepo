f0632917361731b3b1a03a561c940f5686d81d1b
mcachia <mcachia1981@gmail.com>
2016-05-30 11:49:30 +0200

    Milestone2-HW

---
+++ src/HomeWork/Milestone2/FileRead.java
@@ -0,0 +1,23 @@
+package HomeWork.Milestone2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by mcachia on 30/05/2016.
+ */
+public class FileRead {
+
+    public void readFromFile() throws IOException {
+        BufferedReader readFile = new BufferedReader(new FileReader("C:\\Users\\mcachia\\Desktop\\test.txt"));
+        String line;
+
+        while ((line = readFile.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        readFile.close();
+
+    }
+}
---
+++ src/HomeWork/Milestone2/MainApplication.java
@@ -0,0 +1,14 @@
+package HomeWork.Milestone2;
+
+import java.io.IOException;
+
+/**
+ * Created by mcachia on 30/05/2016.
+ */
+public class MainApplication {
+    public static void main(String[] args) throws IOException {
+        FileRead file = new FileRead();
+        file.readFromFile();
+    }
+}
+
